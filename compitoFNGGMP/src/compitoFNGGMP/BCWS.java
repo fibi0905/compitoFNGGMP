e650b0574e70238220c723e71058fdcb91a93167
matteo.piras <matteo.piras@host.docker.internal>
2023-11-13 11:36:49 +0100

    upload BCWS
    Merge branch 'master' of https://github.com/fibi0905/compitoFNGGMP.git

---
+++ compitoFNGGMP/src/compitoFNGGMP/BCWS.java
@@ -0,0 +1,27 @@
+package compitoFNGGMP;
+
+public class BCWS {
+	
+	private int val;
+	
+	BCWS(){
+		
+		val = 0;
+		
+	}
+
+	BCWS(int val){
+		
+		this.val = val;
+		
+	}
+
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+
+}
