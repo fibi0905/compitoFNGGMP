458aa19ceee64678c647d4bf755cbd17d58e5458
gabriele.guadalupi <gabriele.guadalupi@host.docker.internal>
2023-11-13 11:39:12 +0100

    Upload BCWP
    a

---
+++ compitoFNGGMP/src/compitoFNGGMP/BCWP.java
@@ -0,0 +1,25 @@
+package compitoFNGGMP;
+
+public class BCWP {
+	private int val;
+	
+	public BCWP() {
+		this.val = 0;
+	}
+
+
+	public BCWP(int val) {
+		super();
+		this.val = val;
+	}
+
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	} 
+	
+}
+
