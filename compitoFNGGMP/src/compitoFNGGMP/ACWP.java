9859b61c67399ee47ace3000188eeaa6f6a01626
Fabio.Nicolosi <andreamoretti621@gmail.com>
2023-11-13 11:29:41 +0100

    upload ACWP

---
+++ compitoFNGGMP/src/compitoFNGGMP/ACWP.java
@@ -0,0 +1,25 @@
+package compitoFNGGMP;
+
+public class ACWP {
+	private int val;
+
+	public ACWP(int val) {
+		super();
+		this.val = val;
+	}
+	
+	public ACWP() {
+		this.val = 0;
+	}
+
+	
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+	
+	
+}
